07cd30fd3d67d06774ddf6ff1886673d8af05f3d
DanieLucas28 <danielcerqueira@live.com>
2021-11-05 01:52:56 -0300

    Atualizações nas funções: adicionando dados para as tabelas do banco

--- kinea.py
+++ kinea.py
@@ -15,6 +15,7 @@ engine = create_engine("mssql+pyodbc:///?odbc_connect={}".format(params))
 
 Base = declarative_base()
 
+#Classes que criam as tabelas e/ou colocam o dado no formato para serem inseridos no banco de dados
 
 class Numeros_spdb(Base):
     __tablename__ = 'numeros_sp'
@@ -43,7 +44,62 @@ class Analise_seg_dorm_sp(Base):
 
     def __repr__(self):
         return self.Data
+    
+class Analise_seg_zona_sp(Base):
+    __tablename__ = 'seg_zona_sp'
+    id = Column(Integer, primary_key=True)
+    Zona = Column(String)
+    Oferta_anterior = Column(Numeric)
+    Lancamentos = Column(Numeric)
+    Vendas = Column(Numeric)
+    Oferta_final = Column(Numeric)
+    VSO_porcentagem = Column(Numeric)
+    Data = Column(String)
 
+    def __repr__(self):
+        return self.Data
+    
+class Analise_seg_area_sp(Base):
+    __tablename__ = 'seg_area_sp'
+    id = Column(Integer, primary_key=True)
+    Area_util = Column(String)
+    Oferta_anterior = Column(Numeric)
+    Lancamentos = Column(Numeric)
+    Vendas = Column(Numeric)
+    Oferta_final = Column(Numeric)
+    VSO_porcentagem = Column(Numeric)
+    Data = Column(String)
+
+    def __repr__(self):
+        return self.Data
+
+class Analise_seg_preco_sp(Base):
+    __tablename__ = 'seg_preco_sp'
+    id = Column(Integer, primary_key=True)
+    Preco = Column(String)
+    Oferta_anterior = Column(Numeric)
+    Lancamentos = Column(Numeric)
+    Vendas = Column(Numeric)
+    Oferta_final = Column(Numeric)
+    VSO_porcentagem = Column(Numeric)
+    Data = Column(String)
+
+    def __repr__(self):
+        return self.Data
+    
+class Analise_seg_mercado_sp(Base):
+    __tablename__ = 'seg_mercado_sp'
+    id = Column(Integer, primary_key=True)
+    Mercado = Column(String)
+    Oferta_anterior = Column(Numeric)
+    Lancamentos = Column(Numeric)
+    Vendas = Column(Numeric)
+    Oferta_final = Column(Numeric)
+    VSO_porcentagem = Column(Numeric)
+    Data = Column(String)
+
+    def __repr__(self):
+        return self.Data
 
 Base.metadata.create_all(engine)
 Session = sessionmaker(bind=engine)
@@ -59,6 +115,7 @@ mes = '08'  # posso iterar por meses#
 data = Ano+mes
 dataformato = f'{mes}/{Ano}'
 path = f'http://www.secovi.com.br/downloads/pesquisas-e-indices/pmi/{Ano}/arquivos/{data}-pmi.pdf'
+
 
 
 def Numeros_sp(path):
@@ -99,7 +156,6 @@ def Numeros_sp(path):
 
     return vm
 
-
 def Analise_segmentada_dormitorios_sp(path):
 
     if Ano > '2020' and data != "202101":
@@ -133,7 +189,6 @@ def Analise_segmentada_dormitorios_sp(path):
             print("mês já contabilizado")
     return seg
 
-
 def Analise_segmentada_zona_sp(path):
 
     #Retirando dados da análise segmentada por qtd de dormitórios em SP
@@ -141,14 +196,30 @@ def Analise_segmentada_zona_sp(path):
     seg = read_pdf(path, pages="9", area=(194.552, 1.063, 309.369, 566.646))
     seg = seg[0]
     seg.set_index('Zona', inplace=True)
+    seg = seg.T
+    seg = seg.drop('Total', 0)
+    lista = [dataformato] * seg.shape[0]
+    seg['Data'] = lista
 
     #transformando o dado
 
     seg.replace(r'\.', '', regex=True, inplace=True)
     seg.replace(r'\,', '.', regex=True, inplace=True)
+    
+    exists = session.query(Analise_seg_zona_sp).filter(
+        Analise_seg_zona_sp.Data == dataformato).all()
+    if not exists:
+        cont = 0
+        for row, column in seg.iterrows():
+            new_row = Analise_seg_zona_sp(Zona=seg.index[cont], Oferta_anterior=float(seg['Oferta anterior'][row]), Lancamentos=float(seg['Lançamentos'][row]), Vendas=float(
+            seg['Vendas'][row]), Oferta_final=float(seg['Oferta final'][row]), VSO_porcentagem=float(seg['VSO (%)'][row]), Data=seg['Data'][row])
+            session.add(new_row)
+            session.commit()
+            cont+= 1
+    else:
+        print("mês já contabilizado")
 
     return seg
-
 
 def Analise_segmentada_area_util_sp(path):
     #Retirando dados da análise segmentada por zona em SP
@@ -156,14 +227,30 @@ def Analise_segmentada_area_util_sp(path):
     seg = read_pdf(path, pages="9", area=(320.001, 1.063, 450.411, 566.646))
     seg = seg[0]
     seg.set_index('Área útil (m2)', inplace=True)
+    seg = seg.T
+    seg = seg.drop('Total', 0)
+    lista = [dataformato] * seg.shape[0]
+    seg['Data'] = lista
 
     #transformando o dado
 
     seg.replace(r'\.', '', regex=True, inplace=True)
     seg.replace(r'\,', '.', regex=True, inplace=True)
+    
+    exists = session.query(Analise_seg_area_sp).filter(
+        Analise_seg_area_sp.Data == dataformato).all()
+    if not exists:
+        cont = 0
+        for row, column in seg.iterrows():
+            new_row = Analise_seg_area_sp(Area_util=seg.index[cont], Oferta_anterior=float(seg['Oferta anterior'][row]), Lancamentos=float(seg['Lançamentos'][row]), Vendas=float(
+            seg['Vendas'][row]), Oferta_final=float(seg['Oferta final'][row]), VSO_porcentagem=float(seg['VSO (%)'][row]), Data=seg['Data'][row])
+            session.add(new_row)
+            session.commit()
+            cont+= 1
+    else:
+        print("mês já contabilizado")
 
     return seg
-
 
 def Analise_segmentada_preco(path):
     #Retirando dados da análise segmentada por preço em SP
@@ -171,14 +258,29 @@ def Analise_segmentada_preco(path):
     seg = read_pdf(path, pages="9", area=(463.168, 0.354, 593.578, 566.646))
     seg = seg[0]
     seg.set_index('Preço (R$)', inplace=True)
-
+    seg = seg.T
+    seg = seg.drop('Total', 0)
+    lista = [dataformato] * seg.shape[0]
+    seg['Data'] = lista
     #transformando o dado
 
     seg.replace(r'\.', '', regex=True, inplace=True)
     seg.replace(r'\,', '.', regex=True, inplace=True)
+    
+    exists = session.query(Analise_seg_preco_sp).filter(
+        Analise_seg_preco_sp.Data == dataformato).all()
+    if not exists:
+        cont = 0
+        for row, column in seg.iterrows():
+            new_row = Analise_seg_preco_sp(Preco=seg.index[cont], Oferta_anterior=float(seg['Oferta anterior'][row]), Lancamentos=float(seg['Lançamentos'][row]), Vendas=float(
+            seg['Vendas'][row]), Oferta_final=float(seg['Oferta final'][row]), VSO_porcentagem=float(seg['VSO (%)'][row]), Data=seg['Data'][row])
+            session.add(new_row)
+            session.commit()
+            cont+= 1
+    else:
+        print("mês já contabilizado")
 
     return seg
-
 
 def Analise_segmentada_mercado(path):
     #Retirando dados da análise segmentada por mercado em SP
@@ -186,18 +288,36 @@ def Analise_segmentada_mercado(path):
     seg = read_pdf(path, pages="9", area=(602.792, 1.063, 717.609, 565.937))
     seg = seg[0]
     seg.set_index('Mercado', inplace=True)
+    seg = seg.T
+    seg = seg.drop('Total', 0)
+    lista = [dataformato] * seg.shape[0]
+    seg['Data'] = lista
 
     #transformando o dado
 
     seg.replace(r'\.', '', regex=True, inplace=True)
     seg.replace(r'\,', '.', regex=True, inplace=True)
+    
+    exists = session.query(Analise_seg_mercado_sp).filter(
+        Analise_seg_mercado_sp.Data == dataformato).all()
+    if not exists:
+        cont = 0
+        for row, column in seg.iterrows():
+            new_row = Analise_seg_mercado_sp(Mercado=seg.index[cont], Oferta_anterior=float(seg['Oferta anterior'][row]), Lancamentos=float(seg['Lançamentos'][row]), Vendas=float(
+            seg['Vendas'][row]), Oferta_final=float(seg['Oferta final'][row]), VSO_porcentagem=float(seg['VSO (%)'][row]), Data=seg['Data'][row])
+            session.add(new_row)
+            session.commit()
+            cont+= 1
+    else:
+        print("mês já contabilizado")
 
     return seg
 
 
-#numeros_do_mes = Numeros_sp(path)
+numeros_do_mes = Numeros_sp(path)
 por_segmento = Analise_segmentada_dormitorios_sp(path)
-# por_zona = Analise_segmentada_zona_sp(path)
-# por_area = Analise_segmentada_area_util_sp(path)
-# por_preco = Analise_segmentada_preco(path)
-# por_mercado = Analise_segmentada_mercado(path)
+por_zona = Analise_segmentada_zona_sp(path)
+por_area = Analise_segmentada_area_util_sp(path)
+por_preco = Analise_segmentada_preco(path)
+por_mercado = Analise_segmentada_mercado(path)
+
